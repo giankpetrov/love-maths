e2d22def4a45d14623bc5736cf70e8eb9145fbde
Gianncarlo Ciampaglia <giank7@gmail.com>
2022-09-21 22:54:23 +0000

    add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,8 @@ function runGame(gameType){
         displayMultiplyQuestion(num1, num2);
     }    else if(gameType === "subtract") {
         displaySubtractQuestion(num1, num2)
+    }   else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2)
     }
     else {
             alert(`Unknown game type: ${gameType}`);
@@ -87,6 +89,8 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, "multiply"];
     }   else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    }   else if (operator === "/") {
+        return [operand1 / operand2, "division"]
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -135,7 +139,7 @@ function displayMultiplyQuestion(operand1, operand2){
 // function 
 function displayDivisionQuestion(operand1, operand2){
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "%";
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
